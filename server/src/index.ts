ecd2eca8f9fa406567043f3b565054b8e32db214
Debabrata Sarkar <sarkardebu087@gmail.com>
2024-12-23 22:52:23 +0530

    missed somthing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,25 @@
 import express, { Express } from "express";
 import userRouter from "./routes/user";
-
+import http from "http";
+import {Server} from "socket.io";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
 
+const server = http.createServer(app);
+export const io = new Server(server)
+
 
 
 app.use("/api/v1", userRouter);
 
-async function server(){
-    app.listen(process.env.PORT, ()=>{
+async function main(){
+    server.listen(process.env.PORT, ()=>{
         console.log(`server is listing on port ${process.env.PORT}`);
         
     })
 }
 
-server();
+main();
